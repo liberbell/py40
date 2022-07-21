32c1a9b1d7be18b81ab9cc768dc20db599115a85
Liberbell <liberbell@gmail.com>
2022-07-22 08:38:27 +0900

    sample9

--- sample.py
+++ sample.py
@@ -38,8 +38,8 @@ app.layout = html.Div(children=[
         id='example-graph',
         figure={
             'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': 'Montréal'},
+                {'x': [1, 2, 3], 'y': [10, 16, 14], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [1, 5, 3], 'type': 'bar', 'name': 'Montréal'},
             ],
             'layout': {
                 'title': 'Dash Data Visualization',
