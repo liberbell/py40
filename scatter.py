dba3275d345f11b092a5af7d010e3d4d3ded7fea
Liberbell <liberbell@gmail.com>
2022-07-22 08:52:30 +0900

    scatter5

--- scatter.py
+++ scatter.py
@@ -18,7 +18,21 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-
+app.layout = html.div([
+    dcc.Graph(
+        id='sample-scatter',
+        figure= {
+            'data':[
+                go.Scatter(
+                    x = x1,
+                    y = y1,
+                    mode = 'markers',
+                    opacity = '0.7',
+                )
+            ]
+        }
+    )
+])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
