13d8c4e8049ed535bd71dcaf11c766d7923c5070
Liberbell <liberbell@gmail.com>
2022-07-23 15:04:45 +0900

    line5

--- line.py
+++ line.py
@@ -13,7 +13,11 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-
+app.layout = html.Div([
+    dcc.Graph(
+        id='sample-line',
+    )
+])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
