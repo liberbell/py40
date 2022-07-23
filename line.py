994dff409d3d7429cc319b9b6c829cd47940c5f8
Liberbell <liberbell@gmail.com>
2022-07-23 15:10:09 +0900

    line7

--- line.py
+++ line.py
@@ -24,7 +24,7 @@ app.layout = html.Div([
                     y=df['MSFT'],
                     mode='lines',
                     opacity=0.5,
-                    maker={
+                    marker={
                         'size':15
                     },
                     name='Microsoft',
