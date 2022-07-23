7764432e8097294d78b4be40259cade29a7c08b1
Liberbell <liberbell@gmail.com>
2022-07-23 15:08:54 +0900

    line6

--- line.py
+++ line.py
@@ -16,6 +16,20 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.layout = html.Div([
     dcc.Graph(
         id='sample-line',
+        figure={
+            'data':[
+                go.Scatter(
+                    x=df['date'],
+                    y=df['MSFT'],
+                    mode='lines',
+                    opacity=0.5,
+                    maker={
+                        'size':15
+                    },
+                    name='Microsoft',
+                )
+            ]
+        }
     )
 ])
 
