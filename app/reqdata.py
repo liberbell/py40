bcf5ab9e57f3eafc15c44fd139d83fedb0894aac
Liberbell <liberbell@gmail.com>
2022-07-25 08:50:45 +0900

    reqdata7

--- app/reqdata.py
+++ app/reqdata.py
@@ -11,3 +11,5 @@ soup = BeautifulSoup(res.text, "html.parser")
 name = soup.select("body > div.row > div > div:nth-child(2) > div > div > div.card-image > span")
 print("Name: " + name[0].string)
 
+students = soup.select("body > div.row > div > div:nth-child(2) > div > div > div.card-action > p.subscribers")
+print(students[0].string)
