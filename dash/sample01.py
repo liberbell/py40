e7efec5b4b61a63fd853ab6b04ffbe1dd490eecd
Liberbell <liberbell@gmail.com>
2022-07-24 14:19:47 +0900

    sample3

--- dash/sample01.py
+++ dash/sample01.py
@@ -7,7 +7,14 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout= html.Div([
-    html.Label('Dropdown')
+    html.Label('Dropdown'),
+    dcc.Dropdown(
+        options=[
+            {'label': "Bob", 'value': "bob"},
+            {'label': "Eric", 'value': "eric"},
+            {'label': "Alex", 'value': "alex"}
+        ]
+    )
 ])
 
 if __name__ == '__main__':
