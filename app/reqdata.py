11dc09e19baf63999cc0e46983bafbb300094f1f
Liberbell <liberbell@gmail.com>
2022-07-25 08:43:36 +0900

    reqdata3

--- app/reqdata.py
+++ app/reqdata.py
@@ -4,4 +4,7 @@ import requests
 URL = "https://scraping-for-beginner.herokuapp.com/udemy"
 
 res = requests.get(URL)
-print(res.status_code)
+# print(res.status_code)
+
+soup = BeautifulSoup(res.text, "html.parser")
+print(soup)
