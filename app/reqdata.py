0c25b792485e97ba7b7921e372ea6c008b34866e
Liberbell <liberbell@gmail.com>
2022-07-25 08:51:23 +0900

    reqdata7

--- app/reqdata.py
+++ app/reqdata.py
@@ -13,3 +13,6 @@ print("Name: " + name[0].string)
 
 students = soup.select("body > div.row > div > div:nth-child(2) > div > div > div.card-action > p.subscribers")
 print(students[0].string)
+
+reviews = soup.select("body > div.row > div > div:nth-child(2) > div > div > div.card-action > p.reviews")
+print(reviews[0].string)
