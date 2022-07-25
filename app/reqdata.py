aa5dee45d8d68eaf826cda1f9e1c8a92e0f49947
Liberbell <liberbell@gmail.com>
2022-07-26 08:24:17 +0900

    reqdata3

--- app/reqdata.py
+++ app/reqdata.py
@@ -14,9 +14,10 @@ print("Name: " + name[0].string)
 students = soup.select("body > div.row > div > div:nth-child(2) > div > div > div.card-action > p.subscribers")
 students_string = students[0].string
 students_split = students_string.split("：")
-print("Student number: " + int(students_split[1]))
+print(type(students_split))
+# print("Student number: " + int(students_split[1]))
 
-reviews = soup.select("body > div.row > div > div:nth-child(2) > div > div > div.card-action > p.reviews")
-reviews_string = reviews[0].string
-reviews_split = reviews_string.split("：")
-print("Review num: ", int(reviews[0].string))
+# reviews = soup.select("body > div.row > div > div:nth-child(2) > div > div > div.card-action > p.reviews")
+# reviews_string = reviews[0].string
+# reviews_split = reviews_string.split("：")
+# print("Review num: ", int(reviews[0].string))
