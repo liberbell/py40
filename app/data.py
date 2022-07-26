9bde9862bd588ee1a4b0012acc42265ddfda1b02
Liberbell <liberbell@gmail.com>
2022-07-27 08:54:40 +0900

    date5

--- app/data.py
+++ app/data.py
@@ -5,11 +5,7 @@ import datetime
 df = pd.read_csv("assets/data.csv")
 # print(type(df["date"][0]))
 
-dates = []
-# print(datetime.datetime.strptime(df["date"][0], "%Y/%m/%d").date())
-for _date in df["date"]:
-    date = datetime.datetime.strptime(_date, "%Y/%m/%d").date()
-    dates.append(date)
+
 
 n_subscribers = df["subscribers"].values
 n_reviews = df["reviews"].values
@@ -19,3 +15,12 @@ diff_subscribers = df["subscribers"].diff().values
 
 diff_reviews = df["reviews"].diff().values
 print(diff_reviews)
+
+df = pd.read_csv("assets/data.csv")
+
+dates = []
+for _date in df["date"]:
+    date = datetime.datetime.strptime(_date, "%Y/%m/%d").date()
+    dates.append(date)
+
+n_subscribers 
