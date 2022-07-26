fac96fb11c5648031ed44bc6f16021ccbc73628e
Liberbell <liberbell@gmail.com>
2022-07-27 08:46:13 +0900

    date3

--- app/data.py
+++ app/data.py
@@ -2,9 +2,10 @@ import pandas as pd
 import datetime
 
 df = pd.read_csv("assets/data.csv")
-print(type(df["date"][0]))
+# print(type(df["date"][0]))
 
-print(datetime.datetime.strptime(df["date"][0], "%Y/%m/%d").date())
+dates = []
+# print(datetime.datetime.strptime(df["date"][0], "%Y/%m/%d").date())
 for _date in df["date"]:
     date = datetime.datetime.strptime(_date, "%Y/%m/%d").date()
-    print(date)
+    dates.append(date)
