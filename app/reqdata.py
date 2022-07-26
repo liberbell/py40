af474fd56b4d6a9c062c14622e0b5d5a438dab82
Liberbell <liberbell@gmail.com>
2022-07-27 08:28:49 +0900

    reqdata2

--- app/reqdata.py
+++ app/reqdata.py
@@ -3,47 +3,36 @@ import requests
 import pandas as pd
 import datetime
 
-URL = "https://scraping-for-beginner.herokuapp.com/udemy"
+def get_udemy_info():
+    URL = "https://scraping-for-beginner.herokuapp.com/udemy"
+    res = requests.get(URL)
 
-res = requests.get(URL)
-# print(res.status_code)
+    soup = BeautifulSoup(res.text, "html.parser")
+    name_data = soup.select("body > div.row > div > div:nth-child(2) > div > div > div.card-image > span")
+    name = name_data[0].string
 
-soup = BeautifulSoup(res.text, "html.parser")
-# print(soup)
-name_data = soup.select("body > div.row > div > div:nth-child(2) > div > div > div.card-image > span")
-name = name_data[0].string
-# print(name)
+    students = soup.select("body > div.row > div > div:nth-child(2) > div > div > div.card-action > p.subscribers")
+    students_string = students[0].string
+    students_split = students_string.split("：")[1]
+    students_num = int(students_split)
 
-students = soup.select("body > div.row > div > div:nth-child(2) > div > div > div.card-action > p.subscribers")
-students_string = students[0].string
-students_split = students_string.split("：")[1]
-students_num = int(students_split)
-# print(type(students_split))
-# print(int(students_split))
-# print("Student number: " + int(students_split))
+    reviews = soup.select("body > div.row > div > div:nth-child(2) > div > div > div.card-action > p.reviews")
+    reviews_string = reviews[0].string
+    reviews_split = reviews_string.split("：")[1]
+    reviews_num = int(reviews_split)
 
-reviews = soup.select("body > div.row > div > div:nth-child(2) > div > div > div.card-action > p.reviews")
-reviews_string = reviews[0].string
-reviews_split = reviews_string.split("：")[1]
-reviews_num = int(reviews_split)
-# print(int(reviews_split))
+    results = {"name": name, "n_students": students_num, "n_reviews": reviews_num}
+    return results
 
-results = {"name": name, "n_students": students_num, "n_reviews": reviews_num}
-print(results)
+def 
 
 df = pd.read_csv("assets/data.csv")
-# print(df.head())
-# print(df.tail())
 
-# print(datetime.datetime.today().strftime('%Y/%-m/%-d'))
 date = datetime.datetime.today().strftime('%Y/%-m/%-d')
 
 subscribers = results['n_students']
 review = results['n_reviews']
 
-# print(subscribers, review)
-
-# print(pd.DataFrame([[date, subscribers, review]], columns=["date", 'subscribers', 'reviews']))
 results = pd.DataFrame([[date, subscribers, review]], columns=["date", 'subscribers', 'reviews'])
 
 df = pd.concat([df, results])
