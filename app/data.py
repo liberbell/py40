b296b0bfa592eed5a46bab9a4f0a060ba5462003
Liberbell <liberbell@gmail.com>
2022-07-27 08:45:26 +0900

    data2

--- app/data.py
+++ app/data.py
@@ -1,5 +1,10 @@
 import pandas as pd
-
+import datetime
 
 df = pd.read_csv("assets/data.csv")
-print(df["date"][0])
+print(type(df["date"][0]))
+
+print(datetime.datetime.strptime(df["date"][0], "%Y/%m/%d").date())
+for _date in df["date"]:
+    date = datetime.datetime.strptime(_date, "%Y/%m/%d").date()
+    print(date)
