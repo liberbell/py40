8ef53b045f1f684031a29b45a26cc944fcacc3cc
Liberbell <liberbell@gmail.com>
2022-07-28 08:36:11 +0900

    view3

--- app/view.py
+++ app/view.py
@@ -5,26 +5,20 @@ import dash
 from dash import dcc
 from dash import html
 
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 df = pd.read_csv("assets/data.csv")
-# print(type(df["date"][0]))
-
-
+dates = []
+for _date in df["date"]:
+    date = datetime.datetime.strptime(_date, "%Y/%m/%d").date()
+    dates.append(date)
 
 n_subscribers = df["subscribers"].values
 n_reviews = df["reviews"].values
 
 diff_subscribers = df["subscribers"].diff().values
-# print(diff_subscribers)
-
 diff_reviews = df["reviews"].diff().values
-print(diff_reviews)
+# print(diff_reviews)
 
-df = pd.read_csv("assets/data.csv")
-
-dates = []
-for _date in df["date"]:
-    date = datetime.datetime.strptime(_date, "%Y/%m/%d").date()
-    dates.append(date)
 
 n_subscribers 
