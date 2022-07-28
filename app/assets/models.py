ea0723c696d986ed351f462c882fbeb8e9beb6f0
Liberbell <liberbell@gmail.com>
2022-07-29 08:45:45 +0900

    models1

--- app/assets/models.py
+++ app/assets/models.py
@@ -1,13 +1,9 @@
-# coding: utf-8
 from sqlalchemy import Column, Integer, String, Boolean, DateTime, Date
 from assets.database import Base
 from datetime import datetime as dt
 
-#Table情報
 class Data(Base):
-    #TableNameの設定
     __tablename__ = "data"
-    #Column情報を設定する
     id = Column(Integer, primary_key=True)
     date = Column(Date, unique=False)
     subscribers = Column(Integer, unique=False)
