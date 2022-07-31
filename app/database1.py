3e2ebb1af8150e2933329d6090cfe56639e47e48
Liberbell <liberbell@gmail.com>
2022-07-31 14:08:36 +0900

    database14

--- app/database1.py
+++ app/database1.py
@@ -1,3 +1,4 @@
+from xmlrpc.client import DateTime
 import assets.models
 import pandas as pd
 from assets.database import db_session
@@ -6,3 +7,5 @@ import datetime
 df = pd.read_csv("assets/data.csv")
 print(type(df.iloc[0, 0]))
 
+date1 = datetime.datetime.strptime(df.iloc[0, 0], '%Y/%m/%d')
+print(date1, type(date1))
