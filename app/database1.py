81cc58e3a69ed32181b25d106a80c69ae4177e39
Liberbell <liberbell@gmail.com>
2022-07-31 14:09:36 +0900

    database15

--- app/database1.py
+++ app/database1.py
@@ -5,7 +5,7 @@ from assets.database import db_session
 import datetime
 
 df = pd.read_csv("assets/data.csv")
-print(type(df.iloc[0, 0]))
+# print(type(df.iloc[0, 0]))
 
-date1 = datetime.datetime.strptime(df.iloc[0, 0], '%Y/%m/%d')
+date1 = datetime.datetime.strptime(df.iloc[0, 0], '%Y/%m/%d').date()
 print(date1, type(date1))
