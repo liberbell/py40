e75f094741b438a1a98317085fdac0a40b372eeb
Liberbell <liberbell@gmail.com>
2022-07-31 14:15:37 +0900

    database18

--- app/database1.py
+++ app/database1.py
@@ -1,5 +1,5 @@
 from xmlrpc.client import DateTime
-import assets.models
+import assets import models
 import pandas as pd
 from assets.database import db_session
 import datetime
@@ -12,4 +12,5 @@ df = pd.read_csv("assets/data.csv")
 
 for index, _df in df.iterrows():
     date = datetime.datetime.strptime(_df['date'], '%Y/%m/%d').date()
+    row = models.Data(date=date, subscribers=_df['subscribers'], reviews=_df['reviews'])
     print(date)
