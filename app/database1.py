a293e9e06205ccf6979b662f92fd43f09b00de7f
Liberbell <liberbell@gmail.com>
2022-07-31 14:06:54 +0900

    database13

--- app/database1.py
+++ app/database1.py
@@ -1,6 +1,8 @@
 import assets.models
 import pandas as pd
 from assets.database import db_session
+import datetime
 
 df = pd.read_csv("assets/data.csv")
-print(df.head())
+print(type(df.iloc[0, 0]))
+
